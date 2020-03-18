5ab464a66fba41630c6bfff01c032d13706d3294
Nicholas <nicholas.computacao@outlook.com>
2020-03-17 22:13:28 -0300

    Nova versão! :)

--- avaliador.py
+++ avaliador.py
@@ -9,7 +9,10 @@ class Janela(Mainfunctions):
         super().__init__()
         self.qntacertos = 0
         self.create_tabela_aluno()
+        self.campo()
+        self.iniciado = False
 
+#construir a interface:
     #método para iniciar introduzir a quantidade de alunos que irão fazer a chamada
     def create_tabela_aluno(self):
         self.lblaluno = Label(text="Insira o último número da chamada")
@@ -21,76 +24,26 @@ class Janela(Mainfunctions):
         self.iniciar_chamada["command"] = self.comecar_avaliacao
         self.chamado = Label(text="Para iniciar a chamada:\nDigite a quantidade de alunos clique no botão", fg='red')
         self.chamado.place(x=325, y=140)
-        self.iniciado = False
 
-    #inicia a av, colocando na tela o principal para a chamada
-    def comecar_avaliacao(self):
-        tamanho = int(self.numerodealunos.get())
-        self.chamada = []
-        for i in range(tamanho):
-            self.chamada.append(i+1)
-        escolhido = choice(self.chamada)
-        self.remover(escolhido)
-        self.chamado["text"] = "Chamada iniciada!\no primeiro escolhido é o número " + str(escolhido)
-        #cria os campos para inserir o nome, o sorteio da tabela e o submeter o teste
-        if not(self.iniciado):
-            self.iniciado = True
-            self.nome = Entry()
-            self.lblnome = Label(text="Nome")
-            self.lblnome.place(x=320, y=180)
-            self.nome.place(x=360, y=180)
-            self.acertos = Label(text="0 Acertos")
-            self.acertos.place(x=370, y=210)
-            self.create_avaliador()
-            self.alunos = []
-            self.acertoalunos = []
-            self.submeter = Button(text="Submeter", command=self.submit)
-            self.submeter.place(x=320, y=240)
-            self.reseta = Button(text="Retornar", command= self.retornar)
-            self.reseta.place(x=400, y=240)
-            self.salvar = Button(text="Salvar em .txt", command=self.save)
-            self.salvar.place(x=360, y=300)
-            self.lblsave = Label(text="Não salvo", fg='red')
-            self.lblsave.place(x=360, y=340)
-
-    def save(self):
-        #salva em arquivo .txt
-        if len(self.alunos) == 0:
-            return
-        file = open("resultados.txt", "w+")
-        file.write("Resultados:\n")
-        for i in range(len(self.alunos)):
-            file.write("{}  =  {}\n".format(self.alunos[i], self.acertoalunos[i]))
-        file.close()
-        self.lblsave["text"] = "Salvo com " + str(len(self.alunos)) + (" alunos" if len(self.alunos)>1 else " aluno")
-        self.lblsave["fg"] = 'green'
-
-    #redefine o número de acertos
-    def retornar(self):
-        self.nome.delete(0, END)
-        self.qntacertos = 0
-        self.acertos["text"] = "0 Acertos"
-
-    #manda as informações de nome e acertos para uma lista, para ser salva no arquivo posteriormente
-    def submit(self):
-        if len(str(self.nome.get())) == 0:
-            return
-        self.alunos.append(str(self.nome.get()))
-        x = 100 *(self.qntacertos/(int(self.quantidade.get())))
-        self.acertoalunos.append(x)
-        self.retornar()
-        
-
-    def atualiza_acertos(self):
-        self.qntacertos += 1
-        self.acertos["text"] = str(self.qntacertos) + " Acertos"
-
-    #remove o aluno com número x da chamada
-    def remover(self, valor):
-        for i in range(len(self.chamada)):
-            if self.chamada[i] == valor:
-                del self.chamada[i]
-                return
+    #campo objetos para avaliação
+    def campo(self):
+        self.nome = Entry()
+        self.lblnome = Label(text="Nome")
+        self.lblnome.place(x=320, y=180)
+        self.nome.place(x=360, y=180)
+        self.acertos = Label(text="0 Acertos")
+        self.acertos.place(x=370, y=210)
+        self.create_avaliador()
+        self.alunos = []
+        self.acertoalunos = []
+        self.submeter = Button(text="Submeter", command=self.submit)
+        self.submeter.place(x=320, y=240)
+        self.reseta = Button(text="Retornar", command= self.retornar)
+        self.reseta.place(x=400, y=240)
+        self.salvar = Button(text="Salvar em .txt", command=self.save)
+        self.salvar.place(x=360, y=300)
+        self.lblsave = Label(text="Não salvo", fg='red')
+        self.lblsave.place(x=360, y=340)
 
     #opções para realizar a chamada oral
     def create_avaliador(self):
@@ -112,41 +65,125 @@ class Janela(Mainfunctions):
         self.acertou.place(x=580, y=180)
         self.errou.place(x=640, y=180)
 
-    #reseta os elementos da chamada de um aluno
-    def resetelementos(self):
-        self.elementos = []
+#métodos:
+    #seleciona o primeiro aluno para a avaliação
+    def comecar_avaliacao(self):
+        if not self.iniciado:
+            tamanho = int(self.numerodealunos.get())
+            self.chamada = []
+            for i in range(tamanho):
+                self.chamada.append(i+1)
+            escolhido = choice(self.chamada)
+            self.remover(escolhido)
+            self.chamado["text"] = "Chamada iniciada!\no primeiro escolhido é o número " + str(escolhido)
 
-    #incrementa um e manda para o próximo item da lista
+    #salva alunos e notas em um arquivo de texto
+    def save(self):
+        #salva em arquivo .txt
+        if len(self.alunos) == 0:
+            return
+        file = open("resultados.txt", "w+")
+        file.write("Resultados:\n")
+        for i in range(len(self.alunos)):
+            file.write("{}  =  {}%\n".format(self.alunos[i], round(self.acertoalunos[i], 2)))
+        file.close()
+        self.lblsave["text"] = "Salvo com " + str(len(self.alunos)) + (" alunos" if len(self.alunos)>1 else " aluno")
+        self.lblsave["fg"] = 'green'
+
+    #redefine o número de acertos e a pessoa que está fazendo o teste
+    def retornar(self):
+        self.nome.delete(0, END)
+        self.qntacertos = 0
+        self.acertos["text"] = "0 Acertos"
+        self.reset_tabela()
+
+    #armazena o nome e a pontuação da pessoa e sorteia um novo número
+    def submit(self):
+        if len(str(self.nome.get())) == 0:
+            return
+        self.alunos.append(str(self.nome.get()))
+        x = 100 *(self.qntacertos/(int(self.quantidade.get())))
+        self.acertoalunos.append(x)
+        self.retornar()
+        self.reset_tabela()
+        if len(self.chamada) == 0:
+            self.chamado["text"] = "todos os alunos foram avaliados!"
+        else:
+            escolhido = choice(self.chamada)
+            self.chamado["text"]= "Número " + str(escolhido) + " Foi selecionado!"
+            self.remover(escolhido)
+
+#métodos da avaliação:       
+    #inicia o teste de um aluno
+    def startav(self):
+        self.resetelementos()
+        self.getelementos()
+        self.reset_tabela()
+        self.qntacertos = 0
+        self.acertos["text"] = "0 Acertos"
+        
+    #mostra um acerto a mais para o aluno
+    def atualiza_acertos(self):
+        self.qntacertos += 1
+        self.acertos["text"] = str(self.qntacertos) + " Acertos"
+
+    #remove o aluno da lista de espera para avaliação
+    def remover(self, valor):
+        for i in range(len(self.chamada)):
+            if self.chamada[i] == valor:
+                del self.chamada[i]
+                return
+
+    #adiciona um acerto no teste de um aluno e mostra o próximo elemento
     def acertou(self):
         if self.qntacertos == int(self.quantidade.get()):
             pass
         else:
-            self.atualiza_acertos()
+            if self.tabela_acertos[self.posicao] == 0:
+                self.atualiza_acertos()
+                self.tabela_acertos[self.posicao] = 1
             self.avancar()
 
-    #recebe todos os valores sorteados
+    #marca o erro e mostra o próximo elemento
+    def errou(self):
+        if self.qntacertos == int(self.quantidade.get()):
+            pass
+        else:
+            if self.tabela_acertos[self.posicao] == 0:
+                self.tabela_acertos[self.posicao] = 1
+            self.avancar()
+            
+#métodos da avaliação
+    #adquire os elementos que irão ser mostrados na avaliação, de forma aleatória
     def getelementos(self):
         numeroderepeticoes = int(self.quantidade.get())
         for i in range(numeroderepeticoes):
             self.elementos.append(choice(self.sorteio))
         self.lblelemento["text"] = str(self.elementos[0])
         self.posicao = 0
-        
-    #inicia o teste de um aluno
-    def startav(self):
-        self.resetelementos()
-        self.getelementos()
-        pass
+        self.reset_tabela()
 
-    #avança para o próximo item
+    #avança para o próximo elemento da avaliação
     def avancar(self):
         if self.posicao == len(self.elementos) - 1:
             return
         self.posicao += 1
         self.lblelemento["text"] = str(self.elementos[self.posicao])
 
-    #regressa ao próximo item
+    #regressa um elemento da avaliação
     def anterior(self):
         if (self.posicao > 0):
             self.posicao -= 1
             self.lblelemento["text"] = str(self.elementos[self.posicao])
+            
+    #zera o número de acertos e erros de alguém, para que o mesmo possa tentar o elemento depois
+    def reset_tabela(self):
+        self.tabela_acertos = []
+        numeroderepeticoes = int(self.quantidade.get())
+        for i in range(numeroderepeticoes):
+            self.tabela_acertos.append(0)
+
+    #remove todos os elementos que estão sendo usados em avaliação
+    def resetelementos(self):
+        self.elementos = []
+
